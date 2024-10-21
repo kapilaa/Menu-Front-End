42939d4d286e4fe666ed242d2b8c6a554b0654d8
Ramkesh <ramkeshse@gmail.com>
2024-10-21 23:50:45 +0530

    code optimized

--- src/features/dashboard/components/MenuPage.js
+++ src/features/dashboard/components/MenuPage.js
@@ -29,7 +29,7 @@ function MenuPage(){
        setListMenus(result1)
       }
 
-      //by default loading script /
+      //------------------------By default loading script---------------------- /
 
       useEffect(()=>{    
         var myTreeView = document.getElementById("my-tree-view");
@@ -62,7 +62,7 @@ function MenuPage(){
             Expand All
         </button>
         <button classname="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white  border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 rounded-full">
-            Collapse All {dataForm.name}
+            Collapse All
         </button>
          <div id="my-tree-view" class="tree-view">
                <NestedCategory data={listMenu} setFormStatus={handleStatusChange} setFormData={FormCheckData} />
